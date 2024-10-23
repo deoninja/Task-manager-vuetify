8ac94ddf036ee6473666b16a4386dc817d645d1b
deo <deoninja@gmail.com>
2024-10-23 22:59:30 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <TaskManagement />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TaskManagement from './components/TaskManagement.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TaskManagement,
+  },
+})
+</script>
---
+++ src/views/Task.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Task />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Task from '../components/TaskManagement.vue'
+</script>
