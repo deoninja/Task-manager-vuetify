f1c7d20be2719cb7c4f6ca56324e8046cd6626f4
deo <deoninja@gmail.com>
2024-10-24 18:44:50 +0800

    Fix: add .env file configurations for typescript

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/AboutView.vue'
+import HomeView from '../views/Task.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_API_URL),
   routes: [
     {
       path: '/',
